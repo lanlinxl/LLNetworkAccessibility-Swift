3b31e2b6a06a339df2257a0579576c1eb89c9053
lanlin <lanlin0806@icloud.com>
2022-12-07 20:05:54 +0800

    网络授权demo

---
+++ LLNetworkAccessibility/LLAccessibilityAlertController.swift
@@ -0,0 +1,133 @@
+//
+//  LLAccessibilityAlertController.swift
+//  test
+//
+//  Created by lzwk_lanlin on 2022/12/2.
+//  Copyright © 2022 Weike. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LLAccessibilityAlertController: UIViewController {
+    /// 弹框背景view
+    private lazy var bgView: UIView = {
+        let bgView = UIView()
+        bgView.layer.cornerRadius = 12
+        bgView.layer.masksToBounds = true
+        bgView.backgroundColor = .white
+        return bgView
+    }()
+    
+    /// 关闭
+    private lazy var closeButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.addTarget(self, action: #selector(closeAction), for: .touchUpInside)
+        return button
+    }()
+
+    /// 主标题
+    private lazy var titleLb: UILabel = {
+        let titleLb = UILabel()
+        titleLb.font = .systemFont(ofSize: 16, weight: .medium)
+        titleLb.textColor = UIColor(red: 51 / 255.0, green: 51 / 255.0, blue: 51 / 255.0, alpha: 1.0)
+        titleLb.textAlignment = .center
+        titleLb.text = "开启网络权限"
+        return titleLb
+    }()
+
+    /// 副标题
+    private lazy var desLabel1: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = UIColor(red: 150 / 255.0, green: 154 / 255.0, blue: 173 / 255.0, alpha: 1.0)
+        label.text = "1.跳转到设置页后，点击 无线数据"
+        return label
+    }()
+    
+    private lazy var desImageView1: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "LLAccessibilityImage_1")
+        return imageView
+    }()
+    
+    private lazy var desLabel2: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = UIColor(red: 150 / 255.0, green: 154 / 255.0, blue: 173 / 255.0, alpha: 1.0)
+        label.text = "2.选中 WLAN 与蜂窝网络"
+        return label
+    }()
+    
+    private lazy var desImageView2: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "LLAccessibilityImage_2")
+        return imageView
+    }()
+    
+    lazy var setButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("立即设置", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.setTitleColor(UIColor.red, for: .normal)
+        button.setTitleColor(UIColor(red: 242 / 255.0, green: 246 / 255.0, blue: 252 / 255.0, alpha: 1.0), for: .highlighted)
+        button.addTarget(self, action: #selector(setButtonClick), for: .touchUpInside)
+        return button
+    }()
+    
+    /// 弹框是否可关闭
+    var closeAlertEnable: Bool = false
+    
+    convenience init(){
+        self.init(nibName: nil, bundle: nil)
+        modalTransitionStyle = .crossDissolve
+        modalPresentationStyle = .overFullScreen
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        setupBaseView()
+    }
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        modalTransitionStyle = .crossDissolve
+        modalPresentationStyle = .overFullScreen
+    }
+
+    func setupBaseView() {
+        view.addSubview(closeButton)
+        closeButton.frame = view.bounds
+
+        view.addSubview(bgView)
+        bgView.addSubview(titleLb)
+        bgView.addSubview(desLabel1)
+        bgView.addSubview(desImageView1)
+        bgView.addSubview(desLabel2)
+        bgView.addSubview(desImageView2)
+        bgView.addSubview(setButton)
+        bgView.frame = CGRect(x: UIScreen.main.bounds.size.width / 2 - 130, y: UIScreen.main.bounds.size.height / 2 - 110, width: 260, height: 220)
+        titleLb.frame = CGRect(x: 0, y: 16, width: 260, height: 20)
+        desLabel1.frame = CGRect(x: 20, y: 50, width: 240, height: 12)
+        desImageView1.frame = CGRect(x: 20, y: desLabel1.frame.maxY + 4, width: 220, height: 32)
+        desLabel2.frame = CGRect(x: 20, y: desImageView1.frame.maxY + 8, width: 220, height: 12)
+        desImageView2.frame = CGRect(x: 20, y: desLabel2.frame.maxY + 4, width: 220, height: 32)
+        let line = UIView()
+        line.backgroundColor = UIColor(red: 242 / 255.0, green: 246 / 255.0, blue: 252 / 255.0, alpha: 1.0)
+        bgView.addSubview(line)
+        line.frame = CGRect(x: 0, y: desImageView2.frame.maxY + 16, width: 260, height: 0.5)
+        setButton.frame = CGRect(x: 0, y: line.frame.maxY , width: 260, height: 44)
+    }
+    
+    @objc func setButtonClick(){
+        if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url){
+            UIApplication.shared.open(url)
+        }else {
+            self.dismiss(animated: true)
+        }
+    }
+    
+    @objc func closeAction() {
+        if closeAlertEnable{
+            view.removeFromSuperview()
+        }
+    }
+}
+
+
---
+++ LLNetworkAccessibility/LLNetworkAccessibility.swift
@@ -0,0 +1,312 @@
+//
+//  SFNetworkAccessibility.swift
+//  test
+//
+//  Created by lzwk_lanlin on 2022/11/30.
+//  Copyright © 2022 Weike. All rights reserved.
+//
+
+import Foundation
+import CoreTelephony
+import SystemConfiguration.CaptiveNetwork
+import UIKit
+
+private extension Notification.Name {
+    static let LLNetworkStateChangedNotification = Notification.Name("LLNetworkAccessibilityChangedNotification")
+}
+
+class LLNetworkAccessibility: NSObject {
+    enum NetworkType: String  {
+        case unknown
+        case offline
+        case wifi
+        case cellular
+    }
+    
+    enum AuthType: String {
+        case checking
+        case unknown
+        case available
+        case restricted
+    }
+    
+    enum AlertType: String  {
+        case none
+        case custom
+    }
+    
+    static let shared = LLNetworkAccessibility()
+    private var alertType: AlertType = .custom
+    private let firstRunFlag = "LLNetworkAccessibilityFirstRunFlag"
+    /// 上次保存的状态
+    private var previousState: AuthType = .checking
+    private var reachabilityRef: SCNetworkReachability?
+    private var cellularData: CTCellularData?
+    private var reachabilityCallBack: SCNetworkReachabilityCallBack?
+    /// 进入前台是否检查网络授权
+    private var checkingWithBecomeActive: Bool = false
+    /// 是否自动弹框
+    private var isAutomaticallyAlert: Bool = false
+    /// 自定义设置弹框
+    private lazy var customAlertController = LLAccessibilityAlertController()
+
+    /// 网络类型切换回调
+    var reachabilityUpdateCallBack: ((_ state: AuthType?) -> Void)?
+    
+    private override init() {
+        super.init()
+        addReachabilityCallBack()
+    }
+}
+
+//MARK: public method
+extension LLNetworkAccessibility{
+    /// 开启检测
+    func start(){
+        setupNetworkAccessibility()
+    }
+    
+    /// 配置提示弹框
+    func configAlertInfo(type: AlertType , closeEnable: Bool = false , tintColor: UIColor = UIColor.red){
+        alertType = type
+        switch type {
+        case .custom:
+            isAutomaticallyAlert = true
+            customAlertController.closeAlertEnable = closeEnable
+            customAlertController.setButton.setTitleColor(tintColor, for: .normal)
+        case .none:
+            isAutomaticallyAlert = false
+        }
+    }
+    
+    /// 停止检测
+    func stopAndClean(){
+        cellularData?.cellularDataRestrictionDidUpdateNotifier = nil
+        cellularData = nil
+        previousState = .checking
+        reachabilityCallBack = nil
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(ensureActive), object: nil)
+        hiddenAlert()
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
+        print("NetworkAccessibility 已清除")
+    }
+    
+    /// 当前的授权状态
+    func getCurrentAuthState() -> AuthType{
+        return previousState
+    }
+}
+
+
+//MARK: private method
+extension LLNetworkAccessibility{
+    // 设置网络授权相关
+    private func setupNetworkAccessibility(){
+        if (isSimulator()) {
+            // 模拟器检测默认通过
+            updateAccessibility(with: .available)
+        }
+        if reachabilityRef != nil || cellularData != nil {
+            startCheck()
+            return
+        }
+        guard let reachabilityRef = SCNetworkReachabilityCreateWithName(nil, "223.5.5.5") , let modeString = CFRunLoopMode.defaultMode?.rawValue else { return }
+        NotificationCenter.default.addObserver(self, selector: #selector(appWillResignActive), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+        
+        self.reachabilityRef = reachabilityRef
+        SCNetworkReachabilityScheduleWithRunLoop(reachabilityRef, CFRunLoopGetCurrent(),modeString)
+    
+        if UserDefaults.standard.value(forKey: firstRunFlag) as? Bool == nil {
+            UserDefaults.standard.set(true, forKey: firstRunFlag)
+            Delay(4) {
+                self.startReachabilityNotifier()
+                self.startCellularDataNotifier()
+            }
+        }else {
+            self.startReachabilityNotifier()
+            self.startCellularDataNotifier()
+        }
+    }
+    
+
+    
+    // 监听用户从 Wi-Fi 切换到 蜂窝数据，或者从蜂窝数据切换到 Wi-Fi，另外当从授权到未授权，或者未授权到授权也会调用该方法
+    private func startReachabilityNotifier(){
+        guard let reachabilityRef = reachabilityRef , let modeString = CFRunLoopMode.defaultMode?.rawValue else { return }
+        let selfPointer = UnsafeRawPointer(Unmanaged.passUnretained(self).toOpaque())
+        var context = SCNetworkReachabilityContext(version: 0, info: UnsafeMutableRawPointer(mutating: selfPointer) , retain: nil, release: nil, copyDescription: nil)
+        if SCNetworkReachabilitySetCallback(reachabilityRef, reachabilityCallBack, &context){
+            SCNetworkReachabilityScheduleWithRunLoop(reachabilityRef, CFRunLoopGetCurrent(), modeString);
+        }
+    }
+    
+    private func addReachabilityCallBack(){
+        // 用户从 Wi-Fi 切换到 蜂窝数据，或者从蜂窝数据切换到 Wi-Fi 的回调
+        reachabilityCallBack = { _ , _ ,info  in
+          if let pointer = UnsafeRawPointer(info){
+              let mySelf = Unmanaged<LLNetworkAccessibility>.fromOpaque(pointer).takeUnretainedValue()
+              mySelf.startCheck()
+           }
+        }
+    }
+
+    private func startCellularDataNotifier(){
+        let cellularData = CTCellularData()
+        self.cellularData = cellularData
+        cellularData.cellularDataRestrictionDidUpdateNotifier = {[weak self] _ in
+            DispatchQueue.main.async {
+                self?.startCheck()
+            }
+        }
+    }
+    
+    // 开始检测
+    private func startCheck(){
+        if (currentReachable()){
+            print("网络可用")
+            return updateAccessibility(with: .available)
+        }
+        guard let cellularData = cellularData else { return }
+        let state = cellularData.restrictedState
+        switch state {
+            case .restricted:
+                /*  若用户是通过蜂窝数据 或 WLAN 上网，走到这里来 说明权限被关闭**/
+                if isUseWifiConnect() || isUseWWANConnect(){
+                    print("网络没授权")
+                    updateAccessibility(with: .restricted)
+                }else {
+                    print("飞行模式")
+                    updateAccessibility(with: .unknown)
+                }
+            case .notRestricted:
+                print("网络可用")
+                updateAccessibility(with: .available)
+            case .restrictedStateUnknown:
+                Delay(0.5) {
+                    self.startCheck()
+                }
+            default:
+                break
+        }
+    }
+    
+    /* currentReachable 若返回的为 YES 则说明：
+     1. 用户选择了 「WALN 与蜂窝移动网」并处于其中一种网络环境下。
+     2. 用户选择了 「WALN」并处于 WALN 网络环境下。
+     **/
+    private func currentReachable() -> Bool{
+        guard let reachabilityRef = reachabilityRef else { return false}
+        var flags = SCNetworkReachabilityFlags()
+        if SCNetworkReachabilityGetFlags(reachabilityRef, &flags){
+            return (flags == .reachable)
+        }
+        return false
+    }
+    
+    /// 是否在使用wifi连接
+    private func isUseWifiConnect() -> Bool{
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        guard getifaddrs(&ifaddr) == 0 , let firstAddr = ifaddr else {
+            return false
+        }
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            // Check for IPV4 or IPV6 interface
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                // Check interface name
+                let name = String(cString: interface.ifa_name)
+                if name == "en0" {
+                    // Convert interface address to a human readable string
+                    var addr = interface.ifa_addr.pointee
+                    var hostName = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(&addr,socklen_t(interface.ifa_addr.pointee.sa_len), &hostName, socklen_t(hostName.count), nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostName)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        if let wifiIP = address , wifiIP.count > 0 {
+            return true
+        }
+        return false
+    }
+    
+    // 是否在使用蜂窝网络连接（3G、4G、5G）
+    private func isUseWWANConnect() -> Bool {
+        guard let reachabilityRef = reachabilityRef else { return false}
+        var flags = SCNetworkReachabilityFlags()
+        if SCNetworkReachabilityGetFlags(reachabilityRef, &flags){
+            print("是否是蜂窝网络：\(flags.contains(.isWWAN))")
+            return flags.contains(.isWWAN)
+        }
+        return false
+    }
+
+   // 更新网络状态
+   private func updateAccessibility(with state: AuthType){
+        if (isAutomaticallyAlert){
+            state == .restricted ? showAlert() : hiddenAlert()
+        }
+        // 状态发生变化
+        if state != previousState {
+            previousState = state
+        }
+        reachabilityUpdateCallBack?(state)
+        NotificationCenter.default.post(name: .LLNetworkStateChangedNotification, object: nil)
+    }
+    
+    /// 延时调用
+    private func Delay(_ time: TimeInterval, queue: DispatchQueue = .main, closure: @escaping () -> Void) {
+        queue.asyncAfter(deadline: .now() + time) { closure() }
+    }
+    
+    /// 是否是模拟器
+    private func isSimulator() -> Bool{
+        #if TARGET_OS_SIMULATOR
+           return true
+        #else
+           return false
+        #endif
+    }
+}
+
+
+// MARK: - Active Check
+extension LLNetworkAccessibility{
+    @objc private func appWillResignActive(){
+        hiddenAlert()
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(ensureActive), object: nil)
+        checkingWithBecomeActive = true
+    }
+    
+    @objc private func appDidBecomeActive(){
+        if checkingWithBecomeActive {
+            checkingWithBecomeActive = false
+            perform(#selector(ensureActive), with: nil, afterDelay: 1.5,inModes: [RunLoop.Mode.common])
+        }
+    }
+    
+     @objc private func ensureActive(){
+         startReachabilityNotifier()
+         startCellularDataNotifier()
+     }
+}
+
+// MARK: - Alert About
+extension LLNetworkAccessibility{
+    // 展示弹框
+    func showAlert(){
+        if customAlertController.view.superview == nil{
+            UIApplication.shared.keyWindow?.addSubview(customAlertController.view)
+            customAlertController.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        }
+    }
+    // 隐藏弹框
+    func hiddenAlert(){
+        customAlertController.view.removeFromSuperview()
+    }
+}
